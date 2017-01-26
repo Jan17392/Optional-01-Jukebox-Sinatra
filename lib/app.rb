9fec382b2082cd0451594a63e1a3db584b52124a
Jan Runo <jan.runo@whu.edu>
2017-01-26 17:02:38 -0200

    fully functioning

--- lib/app.rb
+++ lib/app.rb
@@ -10,23 +10,70 @@ end
 DB = SQLite3::Database.new('lib/db/jukebox.sqlite')
 
 get "/" do
-  # TODO: Gather all artists to be displayed on home page
-  erb :home # Will render views/home.erb file (embedded in layout.erb)
+  @artists = DB.execute("SELECT name, id FROM artists")
+  @artists.map! do |artist|
+    artist = {
+      name: artist[0],
+      id: artist[1]
+    }
+  end
+  erb :home
 end
 
-get "/artist" do
-  # TODO: Gather all artists to be displayed on home page
-  erb :artist # Will render views/home.erb file (embedded in layout.erb)
+
+get "/artist/:id" do
+  @artist_parameter = params[:id]
+  @artist = DB.execute("
+    SELECT *
+    FROM artists
+    JOIN albums ON albums.artist_id = artists.id
+    WHERE artists.id = '#{@artist_parameter}'
+    ")
+  @artist.map! do |album|
+    album = {
+      creator: album[1],
+      id: album[2],
+      title: album[3]
+    }
+  end
+  erb :artist
 end
 
-get "/album" do
-  # TODO: Gather all artists to be displayed on home page
-  erb :album # Will render views/home.erb file (embedded in layout.erb)
+
+
+get "/album/:id" do
+  @album_parameter = params[:id]
+  @album = DB.execute("
+    SELECT *
+    FROM albums
+    JOIN tracks ON tracks.album_id = albums.id
+    WHERE albums.id = '#{@album_parameter}'
+    ")
+  @album.map! do |track|
+    track = {
+      creator: track[1],
+      id: track[0],
+      title: track[7]
+    }
+    #p "$"*100
+    #p @album
+  end
+  erb :album
 end
 
-get "/track" do
-  # TODO: Gather all artists to be displayed on home page
-  erb :track # Will render views/home.erb file (embedded in layout.erb)
+get "/track/:id" do
+    @track_parameter = params[:id]
+    @track = DB.execute("SELECT * FROM tracks WHERE id = '#{@track_parameter}'")
+    p "$"*100
+    p @track
+
+    @track = {
+      creator: @track[0][5],
+      id: @track[0][0],
+      title: @track[0][4]
+    }
+
+  erb :track
 end
 
 # Then:
